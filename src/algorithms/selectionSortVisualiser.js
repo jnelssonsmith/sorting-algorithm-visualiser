cfd9aafb696af67fbfe333555962056f0a4e84c8
Joshua-Xavier <j.nelssonsmith@gmail.com>
2019-11-10 19:38:24 +1100

    Refactors selection sort to use new data structures

--- src/algorithms/selectionSortVisualiser.js
+++ src/algorithms/selectionSortVisualiser.js
@@ -1,120 +1,61 @@
+import SortingVisualisation from '../models/SortingVisualisation';
+
 /**
  * Selection sort
  * @param {number[]} items - The number array to be sorted using selection sort
  */
 const selectionSortVisualiser = (items) => {
-  let frames = [];
-  let orderedItems = [];
-  let comparisonCount = 0;
-  let swapCount = 0;
-
-  // push initial frame
-  frames.push({
-    positioning: items,
-    comparison: [],
-    swappers: [],
-    highlight: [],
-    ordered: [...orderedItems],
-    comparisonCount: comparisonCount,
-    swapCount: swapCount
-  })
+  const visualisation = new SortingVisualisation(items, 'Swaps');
 
   let mutableArr = items.map(i => i);
   for (let i = 0; i < mutableArr.length; i++) {
     let minElement = mutableArr[0 + i];
     let minIndex = 0 + i;
 
-    frames.push({
-      positioning: [...mutableArr],
-      comparison: [],
-      swappers: [],
+    visualisation.createFrame({
       highlight: [minIndex],
-      ordered: [...orderedItems],
-      comparisonCount: comparisonCount,
-      swapCount: swapCount
     })
   
     for (let k = 0 + i + 1; k < mutableArr.length; k++) {
 
       // we are comparing the current min with the rest of the array
-      frames.push({
-        positioning: [...mutableArr],
+      visualisation.createFrame({
         comparison: [minIndex, k],
-        swappers: [],
-        highlight: [],
-        ordered: [...orderedItems],
-        comparisonCount: comparisonCount,
-        swapCount: swapCount
       })
 
       let comparisonEl = mutableArr[k];
 
-      comparisonCount += 1;
+      visualisation.incrementComparisons();
       if (comparisonEl < minElement) {
-        frames.push({
-          positioning: [...mutableArr],
-          comparison: [],
-          swappers: [minIndex, k],
-          highlight: [],
-          ordered: [...orderedItems],
-          comparisonCount: comparisonCount,
-          swapCount: swapCount
+        visualisation.createFrame({
+          operation: [minIndex, k],
         })
 
         minIndex = k;
         minElement = comparisonEl;
-
-        frames.push({
-          positioning: [...mutableArr],
-          comparison: [],
-          swappers: [],
-          highlight: [minIndex],
-          ordered: [...orderedItems],
-          comparisonCount: comparisonCount,
-          swapCount: swapCount
-        })
       }
     }
 
     if (minIndex !== i) {
-      frames.push({
-        positioning: [...mutableArr],
-        comparison: [],
-        swappers: [minIndex, i],
-        highlight: [],
-        ordered: [...orderedItems],
-        comparisonCount: comparisonCount,
-        swapCount: swapCount
+      visualisation.createFrame({
+        operation: [minIndex, i],
       })
 
       swapElements(mutableArr, minIndex, i);
-      swapCount += 1;
+      visualisation.incrememntOperations();
       
-      frames.push({
-        positioning: [...mutableArr],
-        comparison: [],
-        swappers: [minIndex, i],
-        highlight: [],
-        ordered: [...orderedItems],
-        comparisonCount: comparisonCount,
-        swapCount: swapCount
+      visualisation.createFrame({
+        updatedPositions: [...mutableArr],
+        operation: [minIndex, i],
       })
     }
 
-    orderedItems.push(i);
-    frames.push({
-      positioning: [...mutableArr],
-      comparison: [],
-      swappers: [],
-      highlight: [],
-      ordered: [...orderedItems],
-      comparisonCount: comparisonCount,
-      swapCount: swapCount
-    })
+    visualisation.addOrderedItem(i);
+    visualisation.createFrame({})
     
   }
 
-  return frames;
+  return visualisation;
 }
 
 export default selectionSortVisualiser;
